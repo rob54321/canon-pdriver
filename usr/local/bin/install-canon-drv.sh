7db0b367a21ed39615deecea0fe577dc893a9281
Robert <robertkey@mighty.co.za>
2026-01-01 17:12:33 +0200

    renamed directory for extraction, improved postinst

---
+++ usr/local/bin/install-canon-drv.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# script to install canon printer drivers
+# this script is run by postinst after a delay.
+echo "========================================================="
+echo "installing canon printer drivers"
+echo "========================================================="
+# wait for lock to become available or timeout in seconds
+TIMEOUT=60;
+LOCK="/var/lib/dpkg/lock-frontend"
+
+# check if lock is available
+fuser "${LOCK}"
+rc=$?
+while [[ $rc -eq 0 && ${TIMEOUT} -gt 0 ]]; do
+	echo "install-canon-drv is waiting for the dpkg lock...timeout = ${TIMEOUT} rc = ${rc}"
+	# decrement timeout
+	TIMEOUT=$((TIMEOUT - 5))
+	# get new rc
+	sleep 5
+	fuser "${LOCK}"
+	rc=$?
+done;
+
+# check if the time out occured or not
+fuser "${LOCK}"
+rc=$?
+if test $rc -eq 0; then
+	# lock not available
+	echo "canon drivers not installed...timed out"
+	exit 1;
+else
+	# operation
+	echo "Lock acquired ... Installing canon drivers"
+	cd /home/robert/canon-pdriver
+	install.sh
+fi
+
+echo "========================================================="
+echo "installing canon drivers has completed"
+echo "========================================================="
+exit 0;
